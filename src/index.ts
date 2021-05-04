94aad5878ee9147fdccfd76ee8d9be5217097d18
David Vargas <dvargas92495@gmail.com>
2021-05-03 21:29:04 -0600

    Logging and relative package json

--- src/index.ts
+++ src/index.ts
@@ -9,13 +9,11 @@ import MiniCssExtractPlugin from "mini-css-extract-plugin";
 const build = (): Promise<number> => {
   return new Promise((resolve, reject) => {
     const srcFiles = fs.readdirSync("./src/");
-    console.log('Found', srcFiles, 'in src');
-    const packageJson = path.join(__dirname, "package.json");
-    console.log('Found package json', packageJson);
-    const name = fs.existsSync(packageJson)
-      ? JSON.parse(fs.readFileSync(packageJson).toString())?.name
-      : repoName.sync();
-    console.log('Decided on extension name', name);
+    console.log("Found", srcFiles, "in src");
+    const name = fs.existsSync("package.json")
+      ? JSON.parse(fs.readFileSync("package.json").toString())?.name
+      : repoName.sync({ cwd: path.resolve(".") });
+    console.log("Decided on extension name", name);
     const entryFile =
       srcFiles.find((s) => /index\.(t|j)s/.test(s)) ||
       srcFiles.find((s) => new RegExp(`${name}\\.(t|j)s`).test(s));
@@ -26,7 +24,7 @@ const build = (): Promise<number> => {
       reject(1);
       return;
     }
-    console.log('Using entry file', entryFile);
+    console.log("Using entry file", entryFile);
     webpack(
       {
         entry: {
