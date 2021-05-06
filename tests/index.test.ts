9ca622b2dfdd564416265374bcc182c4ad101e57
David Vargas <dvargas92495@gmail.com>
2021-05-06 15:26:40 -0600

    fix test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,9 +2,9 @@ import run from "../src";
 
 test("Runs Build", (done) => {
   jest.setTimeout(120000);
-  process.chdir('tests/mock');
-  run('build').then((code) => {
+  process.chdir("tests/mock");
+  run("build", []).then((code) => {
     expect(code).toBe(0);
     done();
-  })
+  });
 });
