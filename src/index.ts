20176277d28db8a09b291adaf56eb294f952c64a
David Vargas <dvargas92495@gmail.com>
2021-05-07 10:30:34 -0600

    trying out webpackHotDevClient

--- src/index.ts
+++ src/index.ts
@@ -35,9 +35,7 @@ const getBaseConfig = (): Promise<
     ? dotenv.parse(fs.readFileSync(".env.local"))
     : {};
   return Promise.resolve({
-    entry: {
-      main: `./src/${entryFile}`,
-    },
+    entry: `./src/${entryFile}`,
     target: "web",
     resolve: {
       modules: ["node_modules", appPath("node_modules")],
@@ -179,11 +177,16 @@ const dev = async ({ port: inputPort }: { port: string }): Promise<number> => {
   return new Promise((resolve, reject) => {
     getBaseConfig()
       .then((baseConfig) => {
+        baseConfig.entry = [
+          require.resolve("react-dev-utils/webpackHotDevClient"),
+          baseConfig.entry as string,
+        ];
         baseConfig.module.rules.push({
           test: /\.js$/,
           enforce: "pre",
           use: ["source-map-loader"],
         });
+        baseConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
         baseConfig.output.publicPath = `http://localhost:${port}/`;
         baseConfig.output.pathinfo = true;
         const compiler = webpack({
@@ -198,6 +201,8 @@ const dev = async ({ port: inputPort }: { port: string }): Promise<number> => {
         const server = new webpackDevServer(compiler, {
           contentBase: appPath("build"),
           host: "127.0.0.1",
+          hot: true,
+          transportMode: "ws",
           disableHostCheck: true,
           publicPath: `http://localhost:${port}/`,
           headers: {
