666dd94f2dfad90b15332b95fb57323debab53eb
David Vargas <dvargas92495@gmail.com>
2021-08-21 16:52:38 -0500

    nulling sync worker

--- src/index.ts
+++ src/index.ts
@@ -1005,8 +1005,15 @@ const lambdas = async ({
       const jsdomPatch: esbuild.Plugin = {
         name: "jsdom-patch",
         setup: (build) => {
-          build.onResolve({ filter: /xhr-sync-worker\.js/ }, (args) => {
-            return { path: args.path, external: true };
+          build.onLoad({ filter: /XMLHttpRequest-impl\.js$/ }, async args => {
+            let contents = await fs.promises.readFile(args.path, 'utf8');
+      
+            contents = contents.replace(
+              'const syncWorkerFile = require.resolve ? require.resolve("./xhr-sync-worker.js") : null;',
+              `const syncWorkerFile = null;`,
+            );
+      
+            return { contents, loader: 'js' };
           });
         },
       };
