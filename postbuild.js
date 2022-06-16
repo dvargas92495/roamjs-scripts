76f02f57e4df03c1287a486a7893235920d857af
David Vargas <dvargas92495@gmail.com>
2022-06-16 18:38:41 -0400

    patch local cypress for now

--- postbuild.js
+++ postbuild.js
@@ -1,2 +1,12 @@
-const fs = require('fs');
-fs.writeFileSync('./dist/default.tsconfig.json', fs.readFileSync('./src/default.tsconfig.json').toString());
+const fs = require("fs");
+fs.writeFileSync(
+  "./dist/default.tsconfig.json",
+  fs.readFileSync("./src/default.tsconfig.json").toString()
+);
+fs.writeFileSync(
+  "./node_modules/local-cypress.index.d.ts",
+  fs
+    .readFileSync("./node_modules/local-cypress.index.d.ts")
+    .toString()
+    .replace(/EventEmitter/, "CyEventEmitter")
+);
