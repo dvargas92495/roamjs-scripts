67effc0a85bd7871a1726e0912f145cf064c58d3
David Vargas <dvargas92495@gmail.com>
2022-08-09 16:35:35 -0400

    API_URL accept outside setting in build.ts

--- src/build.ts
+++ src/build.ts
@@ -71,7 +71,7 @@ const build = ({
   if (depot) {
     process.env.ROAM_MARKETPLACE = "true";
     process.env.ROAM_DEPOT = "true";
-    process.env.API_URL = "https://lambda.roamjs.com";
+    process.env.API_URL = process.env.API_URL || "https://lambda.roamjs.com";
   } else {
     process.env.ROAM_MARKETPLACE = "";
     process.env.ROAM_DEPOT = "";
