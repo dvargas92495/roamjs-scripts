86fce47d32e7d6e0883db8a8aa4e873234c5bc0e
David Vargas <dvargas92495@gmail.com>
2022-09-18 17:03:08 -0400

    0.23.1

--- src/common/args.ts
+++ src/common/args.ts
@@ -26,9 +26,12 @@ const args = ({ env = [], ...params }: CliArgs = {}): CliArgs => {
       "tslib=window.TSLib",
     ],
     out: "extension",
-    env: ["ROAM_DEPOT", "BLUEPRINT_NAMESPACE", "ROAMJS_EXTENSION_ID", "API_URL"].concat(
-      env
-    ),
+    env: [
+      "ROAM_DEPOT",
+      "BLUEPRINT_NAMESPACE",
+      "ROAMJS_EXTENSION_ID",
+      "API_URL",
+    ].concat(env),
     mirror: ".",
     format: "esm",
     ...params,
