28a6a5e38d33724199e08f2cad73dbe5e3d70841
David Vargas <dvargas92495@gmail.com>
2022-09-18 16:17:45 -0400

    remove postinstall

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-fs.writeFileSync(
-  "./node_modules/local-cypress/index.d.ts",
-  fs
-    .readFileSync("./node_modules/local-cypress/index.d.ts")
-    .toString()
-    .replace(/EventEmitter/g, "CyEventEmitter")
-);
