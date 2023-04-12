2284ae01b117c73c7e339a1c93ee75acfe9cbfc2
David Vargas <dvargas92495@gmail.com>
2023-04-12 19:21:24 -0400

    samepage/scripts@0.61.5

--- src/test.ts
+++ src/test.ts
@@ -3,15 +3,11 @@ import labsTest from "@samepage/scripts/test";
 
 const test = ({
   forward,
-  env = [],
 }: {
   forward?: string | string[];
-  env?: string | string[];
 }): Promise<number> => {
   return labsTest({
-    ...args({
-      env: ["ROAM_PASSWORD", "ROAM_USERNAME"].concat(env),
-    }),
+    ...args(),
     forward,
     path: "roam",
   });
